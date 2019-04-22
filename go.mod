24ec7134b8e71f4420b2ba3566e14fb8af54ea82
Louise van der Lith <louisevanderlith@gmail.com>
2019-04-22 12:09:46 +0200

    gitflow-feature-stash: modules_ui

--- go.mod
+++ go.mod
@@ -3,5 +3,8 @@ module github.com/louisevanderlith/cars
 require (
 	github.com/astaxie/beego v1.11.1
 	github.com/louisevanderlith/husk v0.6.21
-	github.com/louisevanderlith/mango v0.2.23
+	github.com/louisevanderlith/mango v0.3.14
+	github.com/louisevanderlith/secure v0.1.1
+	gopkg.in/check.v1 v1.0.0-20180628173108-788fd7840127 // indirect
+	gopkg.in/yaml.v2 v2.2.2 // indirect
 )
--- routers/router.go
+++ routers/router.go
@@ -5,18 +5,26 @@ import (
 	"github.com/louisevanderlith/cars/controllers"
 	"github.com/louisevanderlith/mango"
 	"github.com/louisevanderlith/mango/control"
+	secure "github.com/louisevanderlith/secure/core"
 )
 
 func Setup(s *mango.Service) {
 	ctrlmap := EnableFilter(s)
 
-	beego.Router("/", controllers.NewHomeCtrl(ctrlmap))
+	siteName := beego.AppConfig.String("defaultsite")
+	theme, err := mango.GetDefaultTheme(ctrlmap.GetInstanceID(), siteName)
+
+	if err != nil {
+		panic(err)
+	}
+
+	beego.Router("/", controllers.NewHomeCtrl(ctrlmap, theme))
 }
 
 func EnableFilter(s *mango.Service) *control.ControllerMap {
 	ctrlmap := control.CreateControlMap(s)
 
-	emptyMap := make(control.ActionMap)
+	emptyMap := make(secure.ActionMap)
 
 	ctrlmap.Add("/", emptyMap)
 
